cef8fc4578295f0109b3daac767d599852267866
Swapnil1530 <patelswapnil1004@gmail.com>
2022-10-18 21:37:43 +0530

    change

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const app = require("./app")
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`server running at port : ${port}`);
-})
+  try {
+    console.log(`server running at port:${port}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
